61c281b602c2f1267359a9f272cad49b329cea28
ValenChiola <tinochiola06@gmail.com>
2021-10-24 23:11:27 -0300

    modify the content of todo and bugs

---
+++ src/context/UIContext.tsx
@@ -0,0 +1,79 @@
+import React, {
+  createContext,
+  ReactNode,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+
+// Styles
+import Styles from "./UIContext.module.css";
+
+const defaultToast: iToast = {
+  message: null,
+  delay: 3,
+  type: "success",
+};
+
+const Context = createContext({} as iContextValues);
+Context.displayName = "UIContext";
+
+// Hook
+export const useUIContext = () => useContext(Context);
+
+// HOC
+export const UIProvider = ({ children }: { children: ReactNode }) => {
+  const [toast, setToast] = useState(defaultToast);
+
+  useEffect(() => {
+    let timeOut = setTimeout(() => setToast(defaultToast), toast.delay * 1000);
+    return () => {
+      clearTimeout(timeOut);
+    };
+  }, [toast]);
+
+  const showToast = (
+    message: iToast["message"],
+    type: iToast["type"] = "success",
+    delay: iToast["delay"] = 3
+  ) => {
+    if (typeof message !== "string") message = String(message);
+    setToast({
+      message,
+      delay,
+      type,
+    });
+  };
+
+  return (
+    <Context.Provider value={{ toast, showToast }}>
+      {children}
+      {
+        <div
+          className={`${Styles.toastNotification} ${Styles[toast.type]}`}
+          onClick={() => setToast(defaultToast)}
+          aria-hidden="true"
+        >
+          {toast.message}
+        </div>
+      }
+    </Context.Provider>
+  );
+};
+
+// Interfaces
+interface iContextValues {
+  // eslint-disable-next-line no-unused-vars
+  toast: iToast;
+  showToast: (
+    message: iToast["message"],
+    type?: iToast["type"],
+    delay?: iToast["delay"]
+  ) => void;
+}
+
+interface iToast {
+  message: string | null;
+  type: "success" | "error" | "info";
+  delay: number;
+}
---
+++ src/services/api.ts
@@ -0,0 +1,81 @@
+import Axios from "axios";
+
+Axios.defaults.baseURL = "http://localhost:8080/api/v1";
+
+export const getTodoById = async (id: string) => {
+  const endpoint = `/todos/${id}`;
+  const { data } = await Axios.get<iGetTodo>(endpoint);
+  return data.data.todo;
+};
+
+export const getAllTodos = async () => {
+  const { data } = await Axios.get<iGetTodos>("/todos");
+  return data.data.todos;
+};
+
+export const saveTodo = async (todo: Partial<iTodoFromApi>) => {
+  try {
+    const response = await Axios.post<iPostTodos>("/todos", todo);
+    return response.data;
+  } catch (error) {
+    return Promise.reject(error);
+  }
+};
+
+export const removeAllTodos = async () => {
+  try {
+    const { data } = await Axios.delete<iDeleteTodos>("/todos");
+    return data.message;
+  } catch (error) {
+    return Promise.reject(error);
+  }
+};
+
+export const removeTodo = async (id: string) => {
+  try {
+    const endpoint = `/todos/${id}`;
+    const { data } = await Axios.delete<iDeleteTodos>(endpoint);
+    return data.message;
+  } catch (error) {
+    return Promise.reject(error);
+  }
+};
+
+export const toggleDoneTodo = async (id: string) => {
+  try {
+    const endpoint = `/todos/toggle-done/${id}`;
+    await Axios.patch(endpoint);
+  } catch (error) {
+    return Promise.reject(error);
+  }
+};
+
+interface iGetTodo {
+  data: {
+    todo: iTodoFromApi;
+  };
+}
+
+interface iGetTodos {
+  data: {
+    todos: iTodoFromApi[];
+  };
+}
+
+interface iPostTodos {
+  data: {
+    todo: iTodoFromApi;
+  };
+  message: string;
+}
+
+interface iDeleteTodos {
+  message: string;
+}
+
+export interface iTodoFromApi {
+  id: string;
+  content: string;
+  done: boolean;
+  createdDate: number;
+}
